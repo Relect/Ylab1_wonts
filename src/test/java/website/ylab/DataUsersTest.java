0a006a0aa25119404f650001c66723c6b1dbf171
Gennadiy <relect@bk.ru>
2024-10-11 09:19:48 +0300

    add test

--- src/test/java/website/ylab/DataUsersTest.java
+++ src/test/java/website/ylab/DataUsersTest.java
@@ -1,14 +1,17 @@
 package website.ylab;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import website.ylab.in.Read;
 import website.ylab.model.User;
 import website.ylab.out.Write;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class DataUsersTest {
 
     @Mock
@@ -17,14 +20,15 @@ public class DataUsersTest {
     Write out;
 
     @Test
-    public void addUserTest() {
+    public void addUserTest1() {
         DataUsers dataUsers = new DataUsers();
         Mockito.when(in.readLn())
                 .thenReturn("Gennady")
                 .thenReturn("relect@bk.ru")
                 .thenReturn("123");
-        Mockito.doNothing().when(out.writeLn(any(String.class)));
-
-
+        dataUsers.addUser(in, out);
+        assertThat(dataUsers.getUsers()).hasSize(1);
     }
+
+
 }
