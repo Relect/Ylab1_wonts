e02cf004cbf0d85e01d13219901c9bd76185c0df
Gennadiy <relect@bk.ru>
2024-10-16 12:25:43 +0300

    fix

--- src/main/java/website/ylab/custom/Freq.java
+++ src/main/java/website/ylab/custom/Freq.java
@@ -1,5 +1,10 @@
 package website.ylab.custom;
 
+/**
+ * Перечисление для частоты привычки
+ * ежедневная,
+ * еженедельная.
+ */
 public enum Freq {
     EVERYDAY,
     EVERYWEEK
--- src/main/java/website/ylab/custom/Status.java
+++ src/main/java/website/ylab/custom/Status.java
@@ -1,5 +1,10 @@
 package website.ylab.custom;
 
+/**
+ * Перечисление для привычки
+ * DONE выполнена хотя бы раз,
+ * DEFFERRED отложена(не выполнена ни разу).
+ */
 public enum Status {
 
     DONE,
