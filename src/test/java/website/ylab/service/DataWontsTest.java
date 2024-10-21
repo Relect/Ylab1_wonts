6d553c2f67c37c3dc3faeb20bbfb3662c4f726d2
Gennadiy <relect@bk.ru>
2024-10-21 17:39:51 +0300

    dataWontsTest

--- src/test/java/website/ylab/service/DataWontsTest.java
+++ src/test/java/website/ylab/service/DataWontsTest.java
@@ -1,17 +1,26 @@
-/** package website.ylab.service;
+package website.ylab.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import website.ylab.custom.Freq;
+import website.ylab.db.DBManager;
 import website.ylab.in.Read;
 import website.ylab.model.User;
 import website.ylab.model.Wont;
 import website.ylab.out.Write;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MockitoExtension.class)
@@ -22,59 +31,120 @@ public class DataWontsTest {
     @Mock
     Write out;
 
-    DataWonts dataWonts;
-    DataUsers dataUsers;
+    static DataWonts dataWonts;
+    static DataUsers dataUsers;
     User user;
+
+    static PostgresContainer container;
+
+    @BeforeAll
+    public static void beforeAll() {
+
+        container = PostgresContainer.getInstance();
+        container.start();
+
+        try (Connection conn = DBManager.getConn()) {
+            conn.createStatement().execute("CREATE SCHEMA new;");
+
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
+            database.setDefaultSchemaName("new");
+            Liquibase liquibase =
+                    new Liquibase("db/changelog/changelog.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update();
+        } catch (LiquibaseException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        container.stop();
+    }
 
     @BeforeEach
     public void beforeEach() {
-        dataUsers = new DataUsers();
         Mockito.when(in.readLn())
-                .thenReturn("Gennady")
-                .thenReturn("relect@bk.ru")
-                .thenReturn("123");
+                .thenReturn(Constants.USER_NAME)
+                .thenReturn(Constants.USER_EMAIL)
+                .thenReturn(Constants.USER_PASSWORD);
+        dataUsers = new DataUsers();
         dataUsers.addUser(in, out);
         dataWonts = new DataWonts(dataUsers);
-        user = dataUsers.users.get("relect@bk.ru");
-        Mockito.when(in.readLn()).thenReturn("чистка зубов")
-                .thenReturn("предотвращает кариес")
-                .thenReturn("1");
 
-
-        dataWonts.addWont(in, out, user);
+        Mockito.when(in.readLn())
+                .thenReturn(Constants.USER_EMAIL)
+                .thenReturn(Constants.USER_PASSWORD);
+        user = dataUsers.login(in, out);
     }
+    @AfterEach
+    public void afterEach() {
+        dataUsers.deleteUser(Constants.USER_EMAIL, out);
+    }
+
     @DisplayName("проверка добавления привычки")
     @Test
     public void addWontsTest() {
 
-        assertThat(user.getWonts().size())
-                .isEqualTo(1);
-        Wont wont = user.getWonts().get(0);
+        Mockito.when(in.readLn()).thenReturn("чистка зубов")
+                .thenReturn("предотвращает кариес")
+                .thenReturn("1");
+        dataWonts.addWont(in, out, user);
 
-        assertThat(wont.getName()).isEqualTo("чистка зубов");
-        assertThat(wont.getInfo()).isEqualTo("предотвращает кариес");
-        assertThat(wont.getFreq()).isEqualTo(Freq.EVERYDAY);
-        assertThat(wont.isDone()).isEqualTo(false);
+        try (Connection conn = DBManager.getConn()) {
+            ResultSet rs = conn.createStatement()
+                    .executeQuery("SELECT count(*) from new.wonts;");
+            rs.next();
+            long count = rs.getLong("count");
+            assertThat(count).isEqualTo(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     @DisplayName("проверка обновления привычки")
     @Test
     public void editWontTest() {
+
+        String infoOld = "предотвращает кариес";
+        String infoNew = "защищает зубы";
         Mockito.when(in.readLn()).thenReturn("чистка зубов")
-                .thenReturn("2").thenReturn("защищает зубы");
+                .thenReturn(infoOld)
+                .thenReturn("1");
+        dataWonts.addWont(in, out, user);
+
+        Mockito.when(in.readLn()).thenReturn("чистка зубов")
+                .thenReturn("2").thenReturn(infoNew);
         dataWonts.editWont(in, out, user);
-        assertThat(user.getWonts().size())
-                .isEqualTo(1);
-        Wont wont = user.getWonts().get(0);
-        assertThat(wont.getInfo()).isEqualTo("защищает зубы");
+
+        try (Connection conn = DBManager.getConn()) {
+            ResultSet rs = conn.createStatement()
+                    .executeQuery("SELECT info from new.wonts " +
+                            "WHERE name = 'чистка зубов';");
+            rs.next();
+            String actual = rs.getString("info");
+            assertThat(actual).isEqualTo(infoNew);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     @DisplayName("проверка удаления привычки")
     @Test
     public void deleteWontTest() {
+        Mockito.when(in.readLn()).thenReturn("чистка зубов")
+                .thenReturn("предотвращает кариес")
+                .thenReturn("1");
+        dataWonts.addWont(in, out, user);
+
         Mockito.when(in.readLn()).thenReturn("чистка зубов");
         dataWonts.deleteWont(in, out, user);
-        assertThat(user.getWonts().size())
-                .isEqualTo(0);
-
+        try (Connection conn = DBManager.getConn()) {
+            ResultSet rs = conn.createStatement()
+                    .executeQuery("SELECT count(*) from new.wonts;");
+            rs.next();
+            long actual = rs.getLong("count");
+            assertThat(actual).isEqualTo(0);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
-*/
